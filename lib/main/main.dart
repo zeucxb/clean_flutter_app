60b50b1e27654b565ffa78100714e1888e09c8b6
Eliseu Codinhoto <zeucxb@gmail.com>
2020-12-19 13:15:00 -0300

    Lesson 14

---
+++ lib/main/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:clean_flutter_app/ui/components/components.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/ui/pages/login_page.dart
@@ -0,0 +1,62 @@
+import 'package:clean_flutter_app/ui/components/components.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            LoginHeader(),
+            Headline1(text: 'Login'),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColorLight,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(
+                            Icons.lock,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Entrar'.toUpperCase(),
+                      ),
+                    ),
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.person),
+                      label: Text('Criar Conta'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
